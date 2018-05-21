a51a18eedc8813f90b9283496da2aeb588b022d2
Xavier Perez <xap5xap@users.noreply.github.com>
2018-05-21 09:31:32 -0500

    buttons

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
+import { Link } from 'react-router';
 
 class Header extends React.Component {
+    renderButtons() {
+        const { loading, user } = this.props.data;
+        if (loading) { return <div></div> }
+        if (user) {
+            return <div>Logout</div>
+        } else {
+            return (
+                <div>
+                    <li>
+                        <Link to="/signup">Signup</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                </div>
+            )
+        }
+    }
+
     render() {
-        console.log(this.props.data)
         return (
-            <div>
-                Header
-        </div>
+            <nav >
+                <div className="nav-wrapper">
+                    <Link to="/" className="brand-logo left">
+                        Home
+                </Link>
+                    <ul className="right">
+                        {this.renderButtons()}
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
--- client/index.js
+++ client/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, Route, hashHistory } from 'react-router';
 import App from './components/App';
 
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
 const client = new ApolloClient({
-  dataIdFromObject: o => o.id
+  dataIdFromObject: o => o.id,
+  networkInterface
 });
 
 const Root = () => {
