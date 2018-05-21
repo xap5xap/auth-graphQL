3ef463526d0e6aa1d1f6f2f86e56e8804c23a8f2
Xavier Perez <xap5xap@users.noreply.github.com>
2018-05-21 07:00:41 -0500

    user type

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://xap:xap@ds229290.mlab.com:29290/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
